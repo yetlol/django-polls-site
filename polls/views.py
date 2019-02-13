49498530f0a7c2bc5d0c162b43fe94dcaf93eeb7
yetlol <yetbing@hotmail.com>
2019-02-13 07:45:45 -0800

    Add polls templates and remove admin template outsider

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_questions_list = Question.objects.order_by('pub_date')[:5]
+   # output = '<br> '.join([p.question_text for p in latest_questions_list])
+    template = loader.get_template('polls/index.html')
+    context = {
+      'latest_questions_list': latest_questions_list
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
-def result(request, question_id):
+
+def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
