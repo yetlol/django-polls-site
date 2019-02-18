56bde90bbcea59b7d04bd7a7ddd126b1c52687e1
yetlol <yetbing@hotmail.com>
2019-02-18 07:47:00 -0800

    Add url namespace and detail templates

--- mysites/urls.py
+++ mysites/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [ 
-    url(r'^polls/', include('polls.urls')),
+    # namespace is use to confirm diff apps or project
+    url(r'^polls/', include('polls.urls'),
+        namespace="polls"),
     url(r'^admin/', include(admin.site.urls)),
 ]
