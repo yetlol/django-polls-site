fb342518d152e29147e2aed3d6dfd141f0089cbb
tanyan <tanyan@kylinos.cn>
2019-02-26 16:56:42 +0800

    Use generic views

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ just record django oprations
 
 1. 添加了 polls 的template 使用 django.template.loader 加载 index.html
 2. HttpResponse(template.render()) 加载数据
+
+2.26
+学习 generic view 
--- polls/views.py
+++ polls/views.py
@@ -2,42 +2,61 @@ from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Question
 
-def index(request):
-    latest_questions_list = Question.objects.order_by('pub_date')[:5]
-    # output = '<br> '.join([p.question_text for p in latest_questions_list])
-    # template = loader.get_template('polls/index.html')
-    context = {
-      'latest_questions_list': latest_questions_list
-    }
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'polls/index.html', context)
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_questions_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def detail(request, question_id):
-    # use shortcuts get_object_or_404() whice is use
-    # models get() function, if not found, will raise 404()
-    question = get_object_or_404(Question, pk=question_id)
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404('Question is not exist.')
-    context = {
-      'question': question
-    }
-    # return HttpResponse("You're looking at question %s." % question_id)
-    return render(request, 'polls/detail.html', context)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+# def index(request):
+#     latest_questions_list = Question.objects.order_by('pub_date')[:5]
+#     # output = '<br> '.join([p.question_text for p in latest_questions_list])
+#     # template = loader.get_template('polls/index.html')
+#     context = {
+#       'latest_questions_list': latest_questions_list
+#     }
+#     # return HttpResponse(template.render(context, request))
+#     return render(request, 'polls/index.html', context)
 
 
-def results(request, question_id):
-    # response = "You're looking at the results of question %s."
-    # return HttpResponse(response % question_id)
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/result.html', {
-      'question': question
-    })
+# def detail(request, question_id):
+#     # use shortcuts get_object_or_404() whice is use
+#     # models get() function, if not found, will raise 404()
+#     question = get_object_or_404(Question, pk=question_id)
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404('Question is not exist.')
+#     context = {
+#       'question': question
+#     }
+#     # return HttpResponse("You're looking at question %s." % question_id)
+#     return render(request, 'polls/detail.html', context)
+
+
+# def results(request, question_id):
+#     # response = "You're looking at the results of question %s."
+#     # return HttpResponse(response % question_id)
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/result.html', {
+#       'question': question
+#     })
 
 
 def vote(request, question_id):
