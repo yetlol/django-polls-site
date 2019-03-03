0e50f70ad6600eb3079b646f55d0d3531cf4a87d
tanyan <tanyan@kylinos.cn>
2019-03-03 21:12:46 +0800

    Add test testing model and views
    with more views need amend test file.

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ just record django oprations
 
 2.26
 学习 generic view 
+
+3.3
+TDD 开发驱动
+写关于 method index detail 相关的测试
+涉及到 assertEqual assertContains asserQuerysetEqual.需要了解更多的 TestCase 的相关函数。
+还可以添加多个测试用例
+例如：
+检查 question 没有 choice 是否发布
+检查 question 有 choice 是否发布
+Django 可以用 LiveServerTestCase 来和selenium 工具结合测试。
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse, Http404, HttpResponseRedirect
@@ -12,12 +13,20 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_questions_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published questions. not
+        including those set to be publised in the future"""
+        # return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any question that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
